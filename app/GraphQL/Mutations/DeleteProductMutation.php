cf98ea6064612762dc33d694216314923ef838f3
hiralcmobio <hiralc.mobio@gmail.com>
2020-03-04 15:55:53 +0530

    GraphQL with laravel

---
+++ app/GraphQL/Mutations/DeleteProductMutation.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Mutation;
+use App\Product;
+
+class DeleteProductMutation extends Mutation
+{
+    protected $attributes = [
+        'name' => 'deleteProduct'
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Product');
+    }
+
+    public function args(): array
+    {
+        return [
+
+            'id' => [
+                'name' => 'id',
+                'type' =>  Type::nonNull(Type::int()),
+            ],
+
+        ];
+    }
+
+    public function resolve($rootValue, array $args)
+    {
+        $product = Product::find($args['id']);
+        if (!$product) {
+            return null;
+        }
+        $product->delete();
+        return $product;
+    }
+
+}
---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'name' => 'Mobile',
+            'description' => 'This is Mobile Description',
+            'quantity' => '4',
+            'price' => '25000',
+            'model_no' => 'M01',
+            'user_id'=>'1'
+        ]);
+
+        Product::create([
+            'name' => 'Refridgeretor',
+            'description' => 'This is Refridgeretor Description',
+            'quantity' => '56',
+            'price' => '100000',
+            'model_no' => 'R013',
+            'user_id'=>'1'
+        ]);
+
+        Product::create([
+            'name' => 'Head phone',
+            'description' => 'This is Head phone Description',
+            'quantity' => '44',
+            'price' => '5000',
+            'model_no' => 'H051',
+            'user_id'=>'1'
+        ]);
+
+        Product::create([
+            'name' => 'TV',
+            'description' => 'This is TV Description',
+            'quantity' => '34',
+            'price' => '62000',
+            'model_no' => 'T071',
+            'user_id'=>'1'
+        ]);
+
+        Product::create([
+            'name' => 'Fan',
+            'description' => 'This is Fan Description',
+            'quantity' => '23',
+            'price' => '22000',
+            'model_no' => 'F067',
+            'user_id'=>'1'
+        ]);
+
+        Product::create([
+            'name' => 'Laptop',
+            'description' => 'This is Laptop Description',
+            'quantity' => '64',
+            'price' => '50000',
+            'model_no' => 'L089',
+            'user_id'=>'1'
+        ]);
+    }
+}
